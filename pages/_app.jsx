3ab9336733ee2dc8302baad82b63b8fad409fb0c
danieltolo1 <53153812+danieltolo1@users.noreply.github.com>
2022-02-10 16:50:08 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,30 @@
+
 import PrivateLayout from '../layout/PrivateLayout';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import Navbar from  '../components/NavBar'
+import Image from 'next/image';
+import test1 from './manPreventivo'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }){
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+
   console.log('estoy en el app');
+  
   return (
+   <div> 
+     
     <SessionProvider session={session}>
-      <PrivateLayout>
-        <Component {...pageProps} />
-      </PrivateLayout>
+      <Navbar/>  
+      <Component {...pageProps} />
+      
     </SessionProvider>
+    
+
+  </div>
+  
+    
   );
 }
 
